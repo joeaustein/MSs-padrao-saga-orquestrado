687c2ca75dbfeac0b2e221e49e9346c6da8111d8
Joelinton Dorte <joelinton.dorte@ufpr.br>
2025-08-31 14:22:41 -0300

    feat(repo): Order/Event repositories/documents e request DTO's

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
@@ -3,14 +3,20 @@ package br.com.microservices.orchestrated.orderservice.core.document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "event")
 public class Event {
+
+    @Id
     private String id;
     private String transactionId;
     private String orderId;
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -1,16 +1,24 @@
 package br.com.microservices.orchestrated.orderservice.core.document;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "order")
 public class Order {
+
+    @Id
     private String id;
     private List<OrderProducts> products;
     private LocalDateTime createdAt;
